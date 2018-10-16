88384ba4e6a10ad96615a73861b3e972a3940107
Terryd12345 <terrencedenning@gmail.com>
2018-10-17 01:33:22 +1100

    updates

---
+++ puzzle.c
@@ -0,0 +1,315 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <math.h>
+
+
+/** 
+ * READ THIS DESCRIPTION
+ *
+ * node data structure: containing state, g, f
+ * you can extend it with more information if needed
+ */
+typedef struct node{
+	int state[16];
+	int g;
+	int f;
+} node;
+
+/**
+ * Global Variables
+ */
+
+// used to track the position of the blank in a state,
+// so it doesn't have to be searched every time we check if an operator is applicable
+// When we apply an operator, blank_pos is updated
+int blank_pos;
+int test = 0;
+int previous_move = -1;
+
+// Initial node of the problem
+node initial_node;
+
+// Statistics about the number of generated and expendad nodes
+unsigned long generated;
+unsigned long expanded;
+
+
+/**
+ * The id of the four available actions for moving the blank (empty slot). e.x.
+ * Left: moves the blank to the left, etc. 
+ */
+
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
+
+/*
+ * Helper arrays for the applicable function
+ * applicability of operators: 0 = left, 1 = right, 2 = up, 3 = down 
+ */
+int ap_opLeft[]  = { 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 };
+int ap_opRight[]  = { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0 };
+int ap_opUp[]  = { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+int ap_opDown[]  = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 };
+int *ap_ops[] = { ap_opLeft, ap_opRight, ap_opUp, ap_opDown };
+
+
+/* print state */
+void print_state( int* s )
+{
+	int i;
+	
+	for( i = 0; i < 16; i++ )
+		printf( "%2d%c", s[i], ((i+1) % 4 == 0 ? '\n' : ' ') );
+}
+      
+void printf_comma (long unsigned int n) {
+    if (n < 0) {
+        printf ("-");
+        printf_comma (-n);
+        return;
+    }
+    if (n < 1000) {
+        printf ("%lu", n);
+        return;
+    }
+    printf_comma (n/1000);
+    printf (",%03lu", n%1000);
+}
+
+/* return the sum of manhattan distances from state to goal */
+int manhattan( int* state )
+{
+	int curr_x, curr_y, dest_x, dest_y, i, sum = 0;
+
+	for(i=0; i<16; i++){
+		curr_x = i % 4;
+		dest_x = state[i] % 4;
+		curr_y = floor(i / 4);
+		dest_y = floor(state[i] / 4);
+		sum += ( abs(curr_x - dest_x) + abs(curr_y - dest_y) );
+	}
+	
+	return( sum );
+	
+}
+
+
+/* return 1 if op is applicable in state, otherwise return 0 */
+int applicable( int op )
+{
+       	return( ap_ops[op][blank_pos] );
+}
+
+
+/* apply operator */
+void apply( node* n, int op )
+{
+	int t;
+
+	//find tile that has to be moved given the op and blank_pos
+	t = blank_pos + (op == 0 ? -1 : (op == 1 ? 1 : (op == 2 ? -4 : 4)));
+
+	//apply op
+	n->state[blank_pos] = n->state[t];
+	n->state[t] = 0;
+	
+	//update blank pos
+	blank_pos = t;
+}
+
+int min(int a, int b){
+	if(a < b){
+		return a;
+	} else {
+		return b;
+	}
+}
+
+int stupid_move(int curr_move){
+	if( (curr_move == LEFT) && (previous_move == RIGHT) ){
+		return 1;
+	} else if ( (curr_move == RIGHT) && (previous_move == LEFT) ){
+		return 1;
+	} else if( (curr_move == UP) && (previous_move == DOWN) ){
+		return 1;
+	} else if( (curr_move == DOWN) && (previous_move == UP) ){
+		return 1;
+	}
+	return 0;
+}
+
+
+/* Recursive IDA */
+node* ida( node* n, int threshold, int* newThreshold )
+{
+	int i;
+	node *new_node = (node*)malloc(sizeof(node));
+	node *r = (node*)malloc(sizeof(node));
+
+	for(i=0; i<4; i++) {
+		if( applicable(i) && !stupid_move(i) ){
+			previous_move = i;
+	
+			apply(n, i);
+			new_node = n;
+			new_node->g = n->g + 1;
+			new_node->f = new_node->g + manhattan(new_node->state);
+			
+
+			if(new_node->f > threshold){
+				*newThreshold = min(threshold, new_node->f);
+			} else {
+				if( manhattan(new_node->state) == 0 ) {
+					return new_node;
+				}
+				r = ida(new_node, threshold, newThreshold);
+				if( r == NULL ){
+					return r;
+				}
+			}
+			
+		}
+	}
+	return( NULL );
+}
+
+int reinitialize_blank_pos(){
+	for(int i=0; i<16; i++){
+		if(initial_node.state[i] == 0){
+			return i;
+		}
+	}
+	return -1;
+}
+
+/* main IDA control loop */
+int IDA_control_loop(  ){
+	node* r = NULL;
+	node* n = NULL;
+	
+	int threshold;
+	int *newThreshold = (int*)malloc(sizeof(int));
+	
+	/* initialize statistics */
+	generated = 0;
+	expanded = 0;
+
+	/* compute initial threshold B */
+	initial_node.f = threshold = manhattan( initial_node.state );
+
+	printf( "Initial Estimate = %d\nThreshold = ", threshold );
+
+	while( r == NULL ){
+		blank_pos = reinitialize_blank_pos();
+		previous_move = -1;
+		n = (node*)malloc(sizeof(node));
+		*n = initial_node;
+		n->g = 0;
+		*newThreshold = INT_MAX;
+		r = ida(n, threshold, newThreshold);
+		if(r == NULL){
+			threshold = *newThreshold;
+		}
+	}
+
+	if(r)
+		return r->g;
+	else
+		return -1;
+}
+
+
+static inline float compute_current_time()
+{
+	struct rusage r_usage;
+	
+	getrusage( RUSAGE_SELF, &r_usage );	
+	float diff_time = (float) r_usage.ru_utime.tv_sec;
+	diff_time += (float) r_usage.ru_stime.tv_sec;
+	diff_time += (float) r_usage.ru_utime.tv_usec / (float)1000000;
+	diff_time += (float) r_usage.ru_stime.tv_usec / (float)1000000;
+	return diff_time;
+}
+
+int main( int argc, char **argv )
+{
+	int i, solution_length;
+
+	/* check we have a initial state as parameter */
+	if( argc != 2 )
+	{
+		fprintf( stderr, "usage: %s \"<initial-state-file>\"\n", argv[0] );
+		return( -1 );
+	}
+
+
+	/* read initial state */
+	FILE* initFile = fopen( argv[1], "r" );
+	char buffer[256];
+
+	if( fgets(buffer, sizeof(buffer), initFile) != NULL ){
+		char* tile = strtok( buffer, " " );
+		for( i = 0; tile != NULL; ++i )
+			{
+				initial_node.state[i] = atoi( tile );
+				blank_pos = (initial_node.state[i] == 0 ? i : blank_pos);
+				tile = strtok( NULL, " " );
+			}		
+	}
+	else{
+		fprintf( stderr, "Filename empty\"\n" );
+		return( -2 );
+
+	}
+       
+	if( i != 16 )
+	{
+		fprintf( stderr, "invalid initial state\n" );
+		return( -1 );
+	}
+
+	/* initialize the initial node */
+	initial_node.g=0;
+	initial_node.f=0;
+
+	print_state( initial_node.state );
+
+
+	/* solve */
+	float t0 = compute_current_time();
+	
+	solution_length = IDA_control_loop();				
+
+	float tf = compute_current_time();
+
+	/* report results */
+	printf( "\nSolution = %d\n", solution_length);
+	printf( "Generated = ");
+	printf_comma(generated);		
+	printf("\nExpanded = ");
+	printf_comma(expanded);		
+	printf( "\nTime (seconds) = %.2f\nExpanded/Second = ", tf-t0 );
+	printf_comma((unsigned long int) expanded/(tf+0.00000001-t0));
+	printf("\n\n");
+
+	/* aggregate all executions in a file named report.dat, for marking purposes */
+	FILE* report = fopen( "report.dat", "a" );
+
+	fprintf( report, "%s", argv[1] );
+	fprintf( report, "\n\tSoulution = %d, Generated = %lu, Expanded = %lu", solution_length, generated, expanded);
+	fprintf( report, ", Time = %f, Expanded/Second = %f\n\n", tf-t0, (float)expanded/(tf-t0));
+	fclose(report);
+	
+	return( 0 );
+}
+
+
